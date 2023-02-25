a7fbaf7a1405b37b867db2035845da4f9d540ac5
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-02-25 22:08:57 +0900

    Initial Commit

---
+++ Compositional Layout/Compositional_LayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Compositional_LayoutApp.swift
+//  Compositional Layout
+//
+//  Created by Seungchul Ha on 2023/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Compositional_LayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
